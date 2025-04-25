71bc225fcb70a7ae89d359eb70ebe22fdf69b8f8
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2025-04-25 14:39:57 +0530

    node

---
+++ express/index.js
@@ -0,0 +1,11 @@
+
+// local,inbuilt,third party module
+const express=require("express")
+const app=express()
+app.get("/test",(req,res)=>{
+    res.send("hello , i'm node js.api..")
+})
+// listen..
+app.listen(8090,()=>{
+    console.log("server is listening on port 8090");
+})
